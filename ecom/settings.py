fd30b03b795fbb88b2d3f46aac0b6ccacf18d259
thenukan <pthenukan07@gmail.com>
2021-10-29 22:18:34 +0530

    add user and login

--- ecom/settings.py
+++ ecom/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
     'api.category',
-    'api.product'
+    'api.product',
+    'api.user'
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+AUTH_USER_MODEL ="user.CustomUser"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
